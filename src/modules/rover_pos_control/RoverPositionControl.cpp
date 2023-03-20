b0ec00e1d930bb84d1f51a26bfdac441beef21e5
Norbert Szulc <norbert@not7cd.net>
2023-03-21 00:58:42 +0100

    Add throttle scaling

--- src/modules/rover_pos_control/RoverPositionControl.cpp
+++ src/modules/rover_pos_control/RoverPositionControl.cpp
@@ -547,12 +547,12 @@ RoverPositionControl::Run()
 		    _control_mode.flag_control_position_enabled ||
 		    _control_mode.flag_control_manual_enabled) {
 
-
+			float scaling = _param_throttle_max.get();
 
 			vehicle_thrust_setpoint_s v_thrust_sp{};
 			v_thrust_sp.timestamp = hrt_absolute_time();
-			v_thrust_sp.xyz[0] = _throttle_control;
-			v_thrust_sp.xyz[1] = _translation_control;
+			v_thrust_sp.xyz[0] = _throttle_control * scaling;
+			v_thrust_sp.xyz[1] = _translation_control * scaling;
 			v_thrust_sp.xyz[2] = 0.0f;
 			_vehicle_thrust_setpoint_pub.publish(v_thrust_sp);
 
@@ -560,7 +560,7 @@ RoverPositionControl::Run()
 			v_torque_sp.timestamp = hrt_absolute_time();
 			v_torque_sp.xyz[0] = 0.f;
 			v_torque_sp.xyz[1] = 0.f;
-			v_torque_sp.xyz[2] = _yaw_control;
+			v_torque_sp.xyz[2] = _yaw_control * scaling;
 			_vehicle_torque_setpoint_pub.publish(v_torque_sp);
 		}
 	}
